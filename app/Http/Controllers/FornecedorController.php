aec844679a44500403a303b6d08c44c108590dbb
Luan garcia <luangarciatodero2007@gmail.com>
2024-07-19 11:35:37 -0300

    adicionado variavel ``telefone`

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -6,8 +6,34 @@ use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
-    public function index(){
-        $fornecedores =[ 0 =>  ['nome' => 'fornecedor 1', 'status' => 'N']];
+    public function index()
+    {
+        $fornecedores = [
+            0 =>  [
+                'nome' => 'fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '0',
+                'ddd' => '11',
+                'telefone' => '0000-0000'
+            ],
+            1 =>  [
+                'nome' => 'fornecedor 2',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '44',
+                'telefone' => '0000-0000'
+            ],
+            2 =>  [
+                'nome' => 'fornecedor 3',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '85',
+                'telefone' => '0000-0000'
+            ]
+        ];
+
+        $msg = isset($fornecedores[1]['cnpj']) ? 'CNPJ informado': 'CNPJ não informado';
+        echo $msg;
         return view('app.fornecedor.index',  ['Fornecedores' => $fornecedores]);
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -12,16 +12,25 @@
 
 
 
-
-Fornecedor: {{ $Fornecedores[0]['nome']}}
-<br />
-Status: {{ $Fornecedores[0]['status']}}
-<br />
-@if(!($Fornecedores[0]['status'] == 'S'))
-    Fornecedor inativo
-@endif 
-<br />
-@unless($Fornecedores[0]['status'] == 'S')
-  Fornecedor inativo
-  @endunless
-<br /> 
+@isset($Fornecedores)
+    Fornecedor: {{ $Fornecedores[0]['nome']}}
+    <br />
+    Status: {{ $Fornecedores[0]['status']}}
+    <br />
+    CNPJ: {{ $Fornecedores[0]['cnpj'] ?? 'Dado não foi preenchido'}}
+    <br /> 
+    Telefone: ( {{ $Fornecedores[0]['ddd'] ?? 'Dado não foi '}} {{ $Fornecedores[0]['telefone'] ?? 'Dado não foi preenchido'}})
+    @switch($Fornecedores[0]['ddd'])
+      @case ('11')
+         São Paulo - SP
+         @break
+      @case ('44')
+         Paraná - PR
+         @break
+      @case ('85')
+         Fortaleza - CE
+         @break
+         @default
+            Estado não identificado
+    @endswitch
+@endisset
