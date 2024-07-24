a7e497da3c2f807024daa556e8c413ffb854b55c
Luan garcia <luangarciatodero2007@gmail.com>
2024-07-24 11:35:55 -0300

    `update-geral`

--- database/migrations/2024_07_24_124816_create_fornecedores_table.php
+++ database/migrations/2024_07_24_124816_create_fornecedores_table.php
@@ -11,23 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('site_contatos', function(Blueprint $table ){
-            $table->timestamps();
+        Schema::create('fornecedores', function (Blueprint $table) {
+            $table->id();
             $table->string('nome', 50);
-            $table->string('telefone', 20);
-            $table->string('email', 80);
-            $table->integer('motivo_contato');
-            $table->text('mesagem');
+            $table->timestamps();
         });
-            
     }
-    
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('site_contatos');
+        Schema::dropIfExists('fornecedores');
     }
 };
